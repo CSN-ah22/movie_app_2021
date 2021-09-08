2792cbc9805747c00862523029927914ee1f78ba
CSN-ah22 <csa2676@naver.com>
2021-09-08 16:51:47 +0900

    index.js 주석 정리

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import ReactDOM from 'react-dom';
 import App from './App';
 
-
-ReactDOM.render(<App />,document.getElementById('root')); //APP.js의 function App를 호출하는것과 같음 //html에 ID값이 root인 태그안에 App function 결과값을 넣어라
+//컴포넌트를 사용합니다
+ReactDOM.render(<App />,document.getElementById('root')); 
+//APP.js의 function App를 호출하는것과 같음 
+//index.html에 ID값이 root인 div 태그안에 App컴포넌트 결과값을 넣게됨
 
 
