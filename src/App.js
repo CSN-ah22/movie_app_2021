10936d25483ca18c5ec34afe84d1d27393508f88
CSN-ah22 <csa2676@naver.com>
2021-09-15 16:51:49 +0900

    key props 추가하기 (04-03 액션 08 완료)

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function Food({name,picture}){
   return (
     <div>
       <h1>I like {name}</h1>
-      <img src={picture} />
+      <img src={picture} alt={name} />
     </div>
 
   );
@@ -16,10 +16,12 @@ function Food({name,picture}){
 
 const foodILike = [ //json 객체입니다
 {
+  id: 1, //key props
   name:'kimchi',
   image: 'https://cdn.imweb.me/thumbnail/20200415/6b6e035658bac.png'
 },
 {
+  id:2, //key props
   name:'Bibimbap',
   image:'https://lh3.googleusercontent.com/proxy/CDMwVW_u0tk2gIRgzZmi4T4MA2d54xaVGOAbRqxxl_F8dtdnNjnxPftP2V_lDwoVRrUWXDNcuHEQbHZIChrSyHrgt6-PrSmHYfORsOcQILDWURciwOqV1YtoA4giGPrt9qIkKcYqnlyIBeKJkCY'
 }
@@ -34,7 +36,7 @@ function App() {
       {/* json 객체의 name을 출력하는 코드입니다 */}
       {
         //---------------------------
-        foodILike.map(dish => (<Food name={dish.name} picture={dish.image} />))
+        foodILike.map(dish => (<Food key={dish.id} name={dish.name} picture={dish.image} />))
         //⏫json객체 foodILike를 map 객체로 전환하여 dish 함수를 이용해 name 값을 Food컴포넌트로 전달한다
         //---------------------------
       }
