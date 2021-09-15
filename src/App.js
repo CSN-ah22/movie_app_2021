e66f138e24d6991bf162b919ea17a4269793c216
CSN-ah22 <csa2676@naver.com>
2021-09-15 14:51:12 +0900

    App.js - props study

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 
+function Food(props){
+  const {fav}=props; //꼭 fav 로 변수선언을 해주어야만 제대로 출력된다
+
+  return <h1>I like {fav}</h1>;
+  
+}
+
 function App() {
   return (
-    <div>Hello React</div>
+    <div>
+      <h1>Hello React</h1>
+      <Food fav="kimchi" />
+      <Food fav="ramen" />
+      </div>
   );
 }
 
