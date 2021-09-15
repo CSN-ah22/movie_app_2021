315afd9e9ea691abe0360154f850b921ceeed1aa
CSN-ah22 <csa2676@naver.com>
2021-09-15 16:21:21 +0900

    App.js -picture 출력하기

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import Potato from "./Potato"
 
 
-function Food({name}){
+function Food({name,picture}){
   //const {fav}=props; //fav 로 변수선언을 해주어서 값을 담고 출력한다
 
-  return <h1>I like {name}</h1>;
+  return (
+    <div>
+      <h1>I like {name}</h1>
+      <img src={picture} />
+    </div>
+
+  );
   
 }
 
 const foodILike = [
 {
   name:'kimchi',
-  iamge: 'https://cdn.imweb.me/thumbnail/20200415/6b6e035658bac.png'
+  image: 'https://cdn.imweb.me/thumbnail/20200415/6b6e035658bac.png'
 },
 {
   name:'Bibimbap',
@@ -22,12 +28,18 @@ const foodILike = [
 
 function App() {
   return (
+    //출력할 내용을 div 안에 적습니다
     <div>
       
+      {/* json 객체의 name을 출력하는 코드입니다 */}
       {
-        foodILike.map(dish => (<Food name={dish.name} />))
+        //json객체 foodILike를 map 객체로 전환하여 dish 함수를 이용해 name 값을 Food컴포넌트로 전달한다
+        foodILike.map(dish => (<Food name={dish.name} picture={dish.image} />))
       }
+
+      {/* 다른 파일의 컴포넌트를 사용하는 코드입니다 */}
       {/* <Potato DDD="you" /> */}
+
       </div>
   );
 }
