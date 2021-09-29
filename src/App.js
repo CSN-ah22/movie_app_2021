77ca1bb4c084c95b68fc9eadad7e9b7fc4c5ae2e
CSN-ah22 <csa2676@naver.com>
2021-09-30 02:39:41 +0900

    5주차 주석 정리 - App.js -

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from "react" //필수
 
+// <클래스 컴포넌트> 
+//index.js에 있는 <ReactDom컴포넌트>와는 다르다
 class App extends React.Component{ 
+    /*생명주기START*/
     //생성자
     constructor(props){
         //super는 부모 클래스의 생성자를 참조하는것
@@ -9,27 +12,28 @@ class App extends React.Component{
     }
 
     
-    componentDidMount(){ //render이 일어나고 누적 카운트가 필요할때 사용되는 함수 = 저장?
+    componentDidMount(){ //render이 일어나고 누적 카운트가 필요할때 사용되는 함수
         console.log('componentDidMount')
     }
 
     componentDidUpdate(){//화면중에 뭔가가 Update되면 실행되는 함수
         console.log('componentDidUpdate...Good bye')
     }
-    
-    //index.js의 component 가 필요없음
+    /*생명주기END*/
+
+    /*state START*/
     state = {
         count: 0
     }
 
 
     add = () =>{
-        //this.state.count = 1
-        this.setState({count: this.state.count+1})
+        //정적으로 초기화 하고 싶다면>>>{count: 1}
+        this.setState(current => ({count: this.state.count+1}))
     }
 
     minus = () =>{
-        this.setState({count: this.state.count-1})
+        this.setState(current => ({count: this.state.count-1}))
     }
 
     //render 함수를 이용해서 브라우저에 출력한다
@@ -45,37 +49,7 @@ class App extends React.Component{
             </div>
         )
     }
+    /*state END*/
 }
 
 export default App
-
-
-/*
-두번째 방법
-import React, {Component} from "react" //필수
-
-class App extends Component{ //index.js의 component 가 필요없음
-    //render 함수를 이용해서 브라우저에 출력한다
-    render(){ 
-        return (
-            <h1>'Hello'</h1>
-        )
-    }
-}
-
-export default App */
-
-/*
-세번째 방법
-import {Component} from "react" //필수
-
-class App extends Component{ //index.js의 component 가 필요없음
-    //render 함수를 이용해서 브라우저에 출력한다
-    render(){ 
-        return (
-            <h1>'Hello'</h1>
-        )
-    }
-}
-
-export default App */
