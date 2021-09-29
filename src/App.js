6a66a2330cd83fe2457b1e71ff9d92fedc731650
CSN-ah22 <csa2676@naver.com>
2021-09-29 14:54:18 +0900

    음식 이미지 경로>> 상대경로 + json 객체에 rating추가

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Potato from "./Potato"
 
 
-function Food({name,picture}){
+function Food({name,picture}){ //props 연습1
   //const {fav}=props; //fav 로 변수선언을 해주어서 값을 담고 출력한다
 
   return (
@@ -18,27 +18,26 @@ const foodILike = [ //json 객체입니다
 {
   id: 1, //key props
   name:'kimchi',
-  image: 'https://cdn.imweb.me/thumbnail/20200415/6b6e035658bac.png'
+  image: 'images/kimchi.png',rating: 5
 },
 {
   id:2, //key props
   name:'Bibimbap',
-  image:'https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Dolsot-bibimbap.jpg/220px-Dolsot-bibimbap.jpg'
+  image:'images/bibimbap.jpg',rating: 4.9
 }
 
 ];
 
-function App() {
+function App() {/* props 연습2 json 객체의 name을 출력하는 코드입니다 */
   return (
     //출력할 내용을 div 안에 적습니다
     <div>
       
-      {/* json 객체의 name을 출력하는 코드입니다 */}
       {
-        //---------------------------
+        
         foodILike.map(dish => (<Food key={dish.id} name={dish.name} picture={dish.image} />))
         //⏫json객체 foodILike를 map 객체로 전환하여 dish 함수를 이용해 name 값을 Food컴포넌트로 전달한다
-        //---------------------------
+        
       }
 
       {/* 다른 파일의 컴포넌트를 사용하는 코드입니다 */}
