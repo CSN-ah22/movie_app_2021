3dffb8147da932bdc7ec6a9f610cf840dd48c0e0
CSN-ah22 <csa2676@naver.com>
2021-10-13 16:32:17 +0900

    id prop 지우고 HTML 수정

--- src/Movie.js
+++ src/Movie.js
@@ -1,17 +1,19 @@
 import PropTyes from 'prop-types'
 
-function Movie({id,title,year,summary,poster}){
+function Movie({title,year,summary,poster}){
     return (
-        <div class="movie-data">
-            <h3 class="movie-title">{title}</h3>
-            <h5 class="movie-year">{year}</h5>
-            <p class="movie-summary">{summary}</p>
+        <div class="movie">
+            <img src={poster} alt={title} title={title} />
+            <div class="movie-data">
+                <h3 class="movie-title" style={{backgroundColor: 'red'}}>{title}</h3>
+                <h5 class="movie-year">{year}</h5>
+                <p class="movie-summary">{summary}</p>
+            </div>
         </div>
     )
 }
 
 Movie.PropTyes = {
-    id: PropTyes.number.isRequired,
     year: PropTyes.string.isRequired,
     title: PropTyes.string.isRequired,
     summary: PropTyes.string.isRequired,
