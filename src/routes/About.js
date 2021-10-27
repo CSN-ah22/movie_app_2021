91842bf02fc4cada4d51edf22ba4e8b9689ce163
CSN-ah22 <csa2676@naver.com>
2021-10-27 17:08:10 +0900

    About.js 수정 및 css 적용

---
+++ src/routes/About.js
@@ -0,0 +1,11 @@
+import './About.css';
+
+function About (){
+    return (
+        <div className="about__container">
+        <span><h1> "Hello? ㅇㅅㅇ" </h1></span>
+        </div>
+    )
+}
+
+export default About;
