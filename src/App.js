037ba79e3069d002930b9974dd70a2dd32a75f5d
CSN-ah22 <csa2676@naver.com>
2021-10-27 15:36:02 +0900

    HashRouter , Route 뼈대 잡기

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import Home from "./routes/Home"
 import "./App.css"
+import { HashRouter , Route} from 'react-router-dom'
 
 function App(){
     return(
-        <Home />
+        <HashRouter>
+            <Route />
+        </HashRouter>
     )
 }
 
