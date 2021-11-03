ae4e6b2b9d9e3f52250f834e015d770016d7871e
CSN-ah22 <csa2676@naver.com>
2021-11-03 16:31:44 +0900

    리다이렉트 구현

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,8 +1,15 @@
-function Detail(props){
-    console.log(props)
-    return (
-    <span>hello</span>
-    )
+import React from "react";
+
+class Detail extends React.Component{
+    componentDidMount(){//Detail 컴포넌트가 죽으면
+        const { location, history }=this.props
+        if (location.state === undefined){
+            history.push('/');
+        }
+    }
+    render() {
+    return <span>hello</span>
+    }
 }
 
 export default Detail;
